ffe5f3565cdcfa5c9f27775f6954dab6f1eb0e00
Abdelrahman0000 <khlyftbdh5@gmail.com>
2022-11-01 16:21:37 +0200

    l

---
+++ java/main.js
@@ -0,0 +1,91 @@
+
+
+
+
+  var swiper = new Swiper(".box-co",  {
+    
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 20,
+
+breakpoints:{
+  640: {
+    slidesPerView: 1,
+  
+  },
+  768: {
+    slidesPerView: 2,
+
+  },
+  1024: {
+    slidesPerView: 3,
+   
+  }
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+pagination: {
+    el: ".swiper-pagination",
+  },
+        });
+
+
+        var swiper = new Swiper(".box-co1",  {
+    
+          loop:true,
+          grabCursor:true,
+          spaceBetween: 3,
+      
+      breakpoints:{
+        640: {
+          slidesPerView: 1,
+        
+        },
+        768: {
+          slidesPerView: 2,
+      
+        },
+        1024: {
+          slidesPerView: 3,
+         
+        }
+      },
+      navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        
+      pagination: {
+          el: ".swiper-pagination",
+        },
+              });
+      
+      
+
+
+
+        // var swiper = new Swiper(".box-co", {
+        //     slidesPerView: 3,
+        //     centeredSlides: true,
+        //     spaceBetween: 30,
+        //     loop:true,
+        //     pagination: {
+        //       el: ".swiper-pagination",
+        //       type: "fraction",
+        //     },
+        //     navigation: {
+        //       nextEl: ".swiper-button-next",
+        //       prevEl: ".swiper-button-prev",
+        //     },
+        //   });
+
+
+
+
+
+
+
+
